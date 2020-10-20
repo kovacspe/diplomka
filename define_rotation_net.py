ca200ebe18f88743e6e7ab3acec18b613c0f0d64
kovacspe <pet.kovacs1@gmail.com>
2020-10-20 14:50:52 +0200

    new model settings

--- define_rotation_net.py
+++ define_rotation_net.py
@@ -6,200 +6,52 @@ import numpy as np
 import math
 
 
-
 filter_dict = {
-        'gauss5x5': np.float32([
-            [0.003765, 0.015019, 0.023792, 0.015019, 0.003765],
-            [0.015019, 0.059912, 0.094907, 0.059912, 0.015019],
-            [0.023792, 0.094907, 0.150342, 0.094907, 0.023792],
-            [0.015019, 0.059912, 0.094907, 0.059912, 0.015019],
-            [0.003765, 0.015019, 0.023792, 0.015019, 0.003765]]),
-        'gauss3x3': np.float32([
-            [1 / 16, 1 / 8, 1 / 16],
-            [1 / 8, 1 / 4, 1 / 8],
-            [1 / 16, 1 / 8, 1 / 16]]
-        ),
-        'laplace5x5': np.outer(np.float32([1, 4, 6, 4, 1]), np.float32([1, 4, 6, 4, 1])) / 256,
+    'gauss5x5': np.float32([
+        [0.003765, 0.015019, 0.023792, 0.015019, 0.003765],
+        [0.015019, 0.059912, 0.094907, 0.059912, 0.015019],
+        [0.023792, 0.094907, 0.150342, 0.094907, 0.023792],
+        [0.015019, 0.059912, 0.094907, 0.059912, 0.015019],
+        [0.003765, 0.015019, 0.023792, 0.015019, 0.003765]]),
+    'gauss3x3': np.float32([
+        [1 / 16, 1 / 8, 1 / 16],
+        [1 / 8, 1 / 4, 1 / 8],
+        [1 / 16, 1 / 8, 1 / 16]]
+    ),
+    'laplace5x5': np.outer(np.float32([1, 4, 6, 4, 1]), np.float32([1, 4, 6, 4, 1])) / 256,
 
-    }
+}
 
-def create_filter(filter_name,in_channels,out_channels):
+
+def create_filter(filter_name, in_channels, out_channels):
     if filter_name not in filter_dict:
         raise AttributeError(f'filter {filter_name} doesnt exist')
     conv_filter = filter_dict[filter_name]
-    conv_filter = np.tile(conv_filter,(in_channels,out_channels,1,1))
-    conv_filter = np.transpose(conv_filter,(3,2,0,1))
+    conv_filter = np.tile(conv_filter, (in_channels, out_channels, 1, 1))
+    conv_filter = np.transpose(conv_filter, (3, 2, 0, 1))
     return conv_filter
 
-# Core - number 0,1,2
-def get_core_params(height,width,input_channels,output_channels,layer_num):
-    # channels 12
-    # input kernel 7
-    # hidden kernel 3 .. rec kernel
-    output_shape = 1
+
+def define_spatial_feature_readout_network(batch_size, height, width, input_channels, neurons, channels, means):
     params = ffnetwork_params(
-        input_dims=[input_channels,height,width], 
-        layer_sizes=[output_channels],
-        layer_types=['conv'], 
-        act_funcs=['softplus'],
-        shift_spacing=2,
+        input_dims=[input_channels, height, width],
+        layer_sizes=[channels, channels, channels, neurons],
+        layer_types=['conv', 'conv', 'conv', 'spatialxfeature'],
+        act_funcs=['softplus', 'softplus', 'lin', 'softplus'],
+        shift_spacing=[1, 1, 1, None],
         reg_list={
-            'd2x':[0.02 if layer_num==0 else 0.01]})
-    params['weights_initializers'] = ['trunc_normal']
-    params['pos_constraint'] = True
-    if layer_num == 0:
-        params['xstim_n']=[0]
-        params['conv_filter_widths']=[7]
-    else:
-        params['xstim_n'] = None
-        params['ffnet_n'] = [layer_num-1]
-        params['conv_filter_widths']=[3]
-    return params
-
-# Filters #3 #4 #5
-def get_lowpass_filter_params(height,width,input_channels,input_num):
-    params = NDNutils.ffnetwork_params(
-        input_dims=[input_channels,height,width], 
-        layer_sizes=[input_channels],
-        shift_spacing=2,
-        conv_filter_widths=[5],
-        layer_types=['conv'], 
-        act_funcs=['lin']
-        )
-    params['ffnet_n']=[input_num]
-    params['xstim_n']=None
-    return params
-
-# Channel combination #9
-def get_combine_chan_params(number_of_neurons):
-    params = NDNutils.ffnetwork_params(
-            input_dims=[3,number_of_neurons,1], 
-            layer_sizes=[number_of_neurons],
-            #normalization=[0], # Check
-            layer_types=['add'], 
-            act_funcs=['relu'],
-            log_activations=True
-            )
-    #params['output_dims'] = [1,number_of_neurons,1]
-    params['weights_initializers'] = ['normal']# Check
-    params['ffnet_n'] = [3]
-    params['xstim_n'] = None
-    return params
-
-
-
-# Sampler #6
-def get_readout_network(channels,height,width,n_neurons,img_net):
-    l1 = 0.1
-    params = ffnetwork_params(
-        input_dims=[channels,height,width],
-        layer_sizes=[n_neurons],
-        layer_types=['normal'], 
-        act_funcs=['softplus'],
-        log_activations=True,
-        reg_list={
-            'l1':[l1]})
-    params['ffnet_n'] = [img_net]
-    params['xstim_n'] = None
-    return params
-
-def define_basic_readout_network(batch_size,height,width,input_channels,neurons,channels,means):
-    params = ffnetwork_params(
-        input_dims=[input_channels,height,width], 
-        layer_sizes=[channels,channels,channels,neurons],
-        layer_types=['conv','conv','conv','normal'], 
-        act_funcs=['softplus','softplus','softplus','softplus'],
-        shift_spacing=[2,2,2,None],
-        reg_list={
-            'd2x':[0.02, 0.01, 0.01, None],
-            'l1': [None, None, None, 0.01]
-            })
-    params['conv_filter_widths'] = [13,5,5,None]
-    params['weights_initializers'] = ['normal','normal','normal','normal']
-    network = NDN.NDN(params,
-        input_dim_list=[[1,height,width]],
-        batch_size=batch_size,
-        noise_dist='poisson')
-    network.log_correlation='filter-low-std-gold'
-    network.networks[-1].layers[-1].biases = means/2
-    fit_vars = network.fit_variables(layers_to_skip=[[]],fit_biases=True)
-    return network,fit_vars
-
-
-def define_spatial_feature_readout_network(batch_size,height,width,input_channels,neurons,channels,means):
-    params = ffnetwork_params(
-        input_dims=[input_channels,height,width], 
-        layer_sizes=[channels,channels,channels,neurons],
-        layer_types=['conv','conv','conv','spatialxfeature'], 
-        act_funcs=['softplus','softplus','lin','softplus'],
-        shift_spacing=[2,2,1,None],
-        reg_list={
-            'd2x':[0.03, None, None, None],
-            'l1': [None, 0.01, 0.01, 0.01]
-            })
-    params['conv_filter_widths'] = [13,5,5,None]
-    params['weights_initializers'] = ['normal','normal','normal','trunc_normal']
-    params['bias_initializers'] = ['zeros','zeros','zeros','trunc_normal']
+            # 'd2x': [0.03, 0.015, 0.015, None],
+            'l1': [None, None, None, 0.04]
+        })
+    params['conv_filter_widths'] = [13, 5, 5, None]
+    params['weights_initializers'] = ['trunc_normal',
+                                      'trunc_normal', 'trunc_normal', 'trunc_normal']
+    params['bias_initializers'] = ['zeros', 'zeros', 'zeros', 'trunc_normal']
     params['pos_constraint'] = [False, False, False, True]
     network = NDN.NDN(params,
-        input_dim_list=[[1,height,width]],
-        batch_size=batch_size,
-        noise_dist='poisson')
-    network.log_correlation='filter-low-std-gold'
-    network.networks[-1].layers[-1].biases = means/2
-    fit_vars = network.fit_variables(layers_to_skip=[[]],fit_biases=True)
-    return network,fit_vars
-
-def define_MEI_network(batch_size,height,width,input_channels,neurons,channels,means):
-    ff_networks = []
-    layers_to_skip = []
-    chan = input_channels
-    out_chans = []
-    curr_h = height
-    curr_w = width
-
-    # Core 0-2
-    for i in range(3):
-        if i==0:
-            ff_networks.append(get_core_params(curr_h,curr_w,1,channels,i))
-            out_chans = channels
-        elif i==2:
-            ff_networks.append(get_core_params(curr_h,curr_w,chan,64,i))
-            out_chans = 64
-        else:
-            ff_networks.append(get_core_params(curr_h,curr_w,chan,2*chan,i))
-            out_chans=2*chan
-        chan = out_chans
-        layers_to_skip.append([])
-        curr_h = math.ceil(curr_h/2)
-        curr_w = math.ceil(curr_w/2)
-
-    # Lowpass filter 3-5
-    #for i in range(3):
-    #    if i==0:
-    #        ff_networks.append(get_lowpass_filter_params(curr_h,curr_w,chan,range(3)))
-    #    else:
-    #        ff_networks.append(get_lowpass_filter_params(curr_h,curr_w,chan,[2+i]))
-    #    layers_to_skip.append([0])
-    #   curr_h=math.ceil(curr_h/2)
-    #    curr_w=math.ceil(curr_w/2)
-
-    #curr_h = height//2
-    #curr_w = width//2
-    # Readout layer
-    ff_networks.append(get_readout_network(chan,curr_h,curr_w,neurons,img_net=2))
-    layers_to_skip.append([])
-    
-    network = NDN.NDN(ff_networks,
-        input_dim_list=[[1,64,36]],
-        batch_size=batch_size,
-        noise_dist='poisson')
-    network.log_correlation='filter-low-std-gold'
-    #network.networks[-1].layers[0].biases = means/2
-    fit_vars = network.fit_variables(layers_to_skip=layers_to_skip,fit_biases=True)
-    
-    return network,fit_vars
-
-
-
-
+                      input_dim_list=[[1, height, width]],
+                      batch_size=batch_size,
+                      noise_dist='poisson')
+    network.log_correlation = 'filter-low-std-gold'
+    network.networks[-1].layers[-1].biases = 0.5 * np.log(np.exp(means) - 1)
+    return network
--- rotation_training.py
+++ rotation_training.py
@@ -1,55 +1,57 @@
+from load_rotation_data import get_data, Dataset
+from define_rotation_net import define_spatial_feature_readout_network
+from NDN3.NDNutils import ffnetwork_params
+from NDN3.NDN import NDN
+from datetime import datetime
+from scipy import stats
+import tensorflow as tf
+import numpy as np
 import argparse
 
 print('Start rotation training script')
-import numpy as np
-import tensorflow as tf
-print('tf imported with version ',tf.__version__)
-import scipy.stats
-from datetime import datetime
+print('tf imported with version ', tf.__version__)
 print('importing NDN...')
 # Import NDN
-from NDN3.NDN import NDN
-from NDN3.NDNutils import ffnetwork_params
 print('NDN imported')
 # Import network architecture
-from define_rotation_net import define_MEI_network, define_spatial_feature_readout_network, define_basic_readout_network 
 print('MEI net imported')
 # Import data loader
-from load_rotation_data import get_data, Dataset
 print('data imported')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size',type=int, default=64,
+parser.add_argument('--batch_size', type=int, default=256,
                     help='Batch size')
-parser.add_argument('--lr',type=float, default=0.002,
+parser.add_argument('--lr', type=float, default=0.002,
                     help='Learning rate')
-parser.add_argument('--epochs',type=int, default=500,
+parser.add_argument('--epochs', type=int, default=2000,
                     help='Number of training epochs')
-parser.add_argument('--channels',type=int, default=4,
-                    help='Number of training epochs')
-parser.add_argument('--loadmodel',type=str, default=None,
-                    help='Number of training epochs')
+parser.add_argument('--channels', type=int, default=16,
+                    help='Number of channels ic convolutional core')
+parser.add_argument('--loadmodel', type=str, default=None,
+                    help='Load model weights')
+parser.add_argument('--name', type=str, default=None,
+                    help='Experiment name')
 parser.add_argument('--gpu', action='store_true',
-                    help='Number of training epochs')
+                    help='Run on gpu')
 args = parser.parse_args()
 
-def reshape_to_NDN(inp):
-    return np.reshape(inp,[-1,np.prod(inp.shape[1:])])
 
-### Tensorboard logdir setting
-name = f'SFreadout-posinit-bs{args.batch_size}-lr{args.lr}-chan{args.channels}-ep{args.epochs}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
-if args.gpu:
-    logdir="output/" +f'gpu-{name}'  
-else:
-    logdir="output/" +  name
+def reshape_to_NDN(inp):
+    return np.reshape(inp, [-1, np.prod(inp.shape[1:])])
+
+
+# Tensorboard logdir setting
+name = f'{args.name}-bs{args.batch_size}-lr{args.lr}-chan{args.channels}-ep{args.epochs}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+
+logdir = "output/" + name
 GPU = args.gpu
 
-### Load data
+# Load data
 # Train set
 data = get_data('data.pkl')
 train_x, train_y = data.train()
-B_train,H,W,C = train_x.shape
-B_train,Neurons = train_y.shape
+B_train, H, W, C = train_x.shape
+B_train, Neurons = train_y.shape
 print(f'Train set: image shape: {train_x.shape}, output shape {train_y.shape}')
 
 # Validation set
@@ -58,83 +60,92 @@ B_val = val_x.shape[0]
 print(f'Validation set size: {B_val}')
 
 # Merge train and validation set
-train_x = np.concatenate((train_x,val_x),axis=0)
-train_y = np.concatenate((train_y,val_y),axis=0)
+train_x = np.concatenate((train_x, val_x), axis=0)
+train_y = np.concatenate((train_y, val_y), axis=0)
 
 train_x = reshape_to_NDN(train_x)
 train_y = reshape_to_NDN(train_y)
 
+val_x = reshape_to_NDN(val_x)
+val_y = reshape_to_NDN(val_y)
 # Count trian responses means
-means = np.mean(train_y,axis=0)/2
+means = np.mean(train_y, axis=0)/2
 
 
 # Test set
 test_x, test_y = data.test()
+print(
+    f'Train+val set: image shape: {train_x.shape}, output shape {train_y.shape}')
 print(f'Test set: image shape: {test_x.shape}, output shape {test_y.shape}')
 test_x = reshape_to_NDN(test_x)
 test_y = reshape_to_NDN(test_y)
 
 
+# Define network
+net = define_spatial_feature_readout_network(
+    args.batch_size, W, H, C, Neurons, args.channels, means)
 
-### Define network
-net,fit_vars = define_spatial_feature_readout_network(args.batch_size,W,H,C,Neurons,args.channels,means)
-
-opt_params = net.optimizer_defaults({},'adam')
+opt_params = net.optimizer_defaults({}, 'adam')
 opt_params['batch_size'] = args.batch_size
 opt_params['learning_rate'] = args.lr
 opt_params['epochs_summary'] = 25
 opt_params['display'] = 1
 opt_params['use_gpu'] = GPU
 opt_params['epochs_training'] = args.epochs
-opt_params['early_stops'] = 0
+#opt_params['early_stop_mode'] = 3
+#opt_params['early_stop'] = 20
+#opt_params['MAPest'] = True
 #opt_params['epochs_ckpt'] = 20
 #opt_params['run_diagnostics'] = True
 
-def evaluate_performance(pred,gold):
-    c = []
-    for i in range(pred.shape[1]):
-        c.append(scipy.stats.pearsonr(np.array(pred)[:,i].flatten(),gold[:,i].flatten())[0])
 
-    print("Number of NaN corrs: ",np.sum(np.nan_to_num(c)==0))
-    return np.mean(np.nan_to_num(c))
+def evaluate_performance(pred, gold):
+    rho = np.zeros(pred.shape[0])
+    for i, (res, pred) in enumerate(zip(gold, pred)):
+        if np.std(res) > 1e-5 and np.std(pred) > 1e-5:
+            rho[i] = stats.pearsonr(res, pred)[0]
+    return rho.mean()
 
-### Train network
-#print(train_x.shape)
-#print(train_y.shape)
-print(opt_params)
-b_v = min(B_val,args.batch_size) 
+
+# Train network
+b_v = min(B_val, args.batch_size)
 validation_to_train = B_val - b_v
 if args.loadmodel is not None:
     net = NDN.load_model(args.loadmodel)
-    print(args.loadmodel,' loaded')
+    print(args.loadmodel, ' loaded')
     pred = net.generate_prediction(test_x)
-    corr = evaluate_performance(pred,test_y)
-    print('correlation',corr)
+    corr = evaluate_performance(pred, test_y)
+    print('correlation', corr)
 
-net.log_correlation='filter-low-std-gold'
+net.log_correlation = 'filter-low-std-gold'
 net.train(
     train_x,
     train_y,
-    train_indxs=np.arange(B_train+validation_to_train),
-    test_indxs=np.arange(B_train+validation_to_train,B_train+validation_to_train+b_v),
-    fit_variables=fit_vars,
+    train_indxs=np.arange(B_train),
+    test_indxs=np.arange(B_train,
+                         B_train+B_val),
     learning_alg='adam',
     opt_params=opt_params,
     output_dir=logdir
-    )
+)
+
 net.save_model(f'./{name}.pkl')
 
-### Evaluation
+# Evaluation
 
 
 #evaluation = net.eval_models(test_x,test_y)
 #rint('MSE: ',np.mean(evaluation))
 
-#Correlation
+# Correlation
+pred = net.generate_prediction(train_x)
+corr = evaluate_performance(pred, train_y)
+print('Train correlation :', corr)
+
+pred = net.generate_prediction(val_x)
+corr = evaluate_performance(pred, val_y)
+print('Train correlation :', corr)
+
 pred = net.generate_prediction(test_x)
-print(pred)
-
-
-corr = evaluate_performance(pred,test_y)
-print(corr)
-
+corr = evaluate_performance(pred, test_y)
+print('Test correlation :', corr)
