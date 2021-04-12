f3fbf5fc036e5895fd48f7ea444e6dffbd59e1af
kovacspe <pet.kovacs1@gmail.com>
2021-04-13 00:28:20 +0200

    New experiment params

--- gan_param_search.py
+++ gan_param_search.py
@@ -1,13 +1,13 @@
 import os
 import sys
-NEURONS = [28,74,66,23,83,53,54,72,50,24]
+NEURONS = [28,74,66,23,83,]#53,54,72,50,24]
 name = sys.argv[1] if len(sys.argv)>1 else 'untitled'
 models = [
     'models/best_dog.pkl',
     #'models/conv-dog-convDoG-c_size15-layersep-reg_h0.01-hidden9-exp_nameconv-dog.pkl'
 ]
 for neuron in NEURONS:
-    for exp in ['000dog', '001dog', '002dog']:
+    for exp in ['000dog', '001dog', '002dog', '003dog', '004dog']:
         os.system(
             f'qsub -q cpu.q -cwd -pe smp 4 -l mem_free=8G,act_mem_free=8G,h_data=20G  \
             -o job_output/gan/o-{neuron}.log \
