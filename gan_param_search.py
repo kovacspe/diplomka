76a54e37f092b78eef7db607c044510424b73771
kovacspe <pet.kovacs1@gmail.com>
2021-05-01 13:03:13 +0200

    exp update

--- gan_param_search.py
+++ gan_param_search.py
@@ -3,13 +3,13 @@ import yaml
 import sys
 
 nets = sys.argv[1:]
-
+with open('utils/experiments.yml','r') as conf_file:
+    loaded_yaml = yaml.load(conf_file)
 for exp in nets:
-    with open('utils/experiments.yml','r') as conf_file:
-        loaded_yaml = yaml.load(conf_file)
     loaded_params = loaded_yaml.get(exp,{})
     neurons = loaded_params['chosen_neurons']
     for neuron in neurons:
+        print(f'Running {exp}-{neuron}')
         os.system(
             f'qsub -q cpu.q -cwd -pe smp 4 -l mem_free=8G,act_mem_free=8G,h_data=20G  \
             -o job_output/gan/o-{neuron}.log \
